6beeace7450c1c8b83d61d0bcdd34eb7f64de15b
Matt Adcock <43476212+MattA-Official@users.noreply.github.com>
2024-02-11 13:55:07 +0000

    Formatting

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,20 +1,23 @@
 <script>
-  import { invoke } from "@tauri-apps/api/core"
+  import { invoke } from "@tauri-apps/api/core";
 
   let name = "";
-  let greetMsg = ""
+  let greetMsg = "";
 
-  async function greet(){
+  async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
+    greetMsg = await invoke("greet", { name });
   }
 </script>
 
 <div>
   <form class="row" on:submit|preventDefault={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <input
+      id="greet-input"
+      placeholder="Enter a name... or else"
+      bind:value={name}
+    />
     <button type="submit">Greet</button>
   </form>
   <p>{greetMsg}</p>
 </div>
-
